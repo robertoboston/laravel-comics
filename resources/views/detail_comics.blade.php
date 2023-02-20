ce96b211a96ea3016fdff986a7b54be6d92a10eb
roberto.boston <roberto.bozzo97@tiscali.it>
2023-02-20 17:44:46 +0100

    add style to detail page

--- resources/views/detail_comics.blade.php
+++ resources/views/detail_comics.blade.php
@@ -7,22 +7,33 @@
 <div class="blue-band">
 </div>
 <div class="container-single-card">
-    <div class="single-card">
-        <img src="{{$single['thumb']}}" alt="{{$single['title']}}">
-    </div>
-    <div class="comic-title">
-        <h1>{{$single['title']}}</h1>
-    </div>
-    <div class="price-section">
-        <div>
-           U.S.-Price {{$single['price']}}
+    <div class="col-80">
+        <div class="single-card">
+            <img src="{{$single['thumb']}}" alt="{{$single['title']}}">
+        </div>
+        <div class="comic-title">
+            <h1>{{$single['title']}}</h1>
+        </div>
+        <div class="price-section">
+            <div>
+               U.S.-Price {{$single['price']}}
+            </div>
+            <div>
+                Available
+            </div>
         </div>
         <div>
-            Available
+            {{$single['description']}}
         </div>
     </div>
-    <div>
-        {{$single['description']}}
+    <div class="col-20">
+        <div class="advertise">
+            <img src="{{Vite::asset('resources/img/adv.jpg')}}" alt="">
+        </div>
     </div>
+</div>
+<div class="description">
+    
+
 </div>
 @endsection
